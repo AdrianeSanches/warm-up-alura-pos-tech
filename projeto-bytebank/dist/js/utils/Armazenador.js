f5a650ba739ae8a3f0b772238f5c0e3bc2765f23
AdrianeSS <adriane_silvasanches@hotmail.com>
2025-08-04 22:16:37 -0300

    feat: generics

--- projeto-bytebank/dist/js/utils/Armazenador.js
+++ projeto-bytebank/dist/js/utils/Armazenador.js
@@ -4,6 +4,7 @@ export class Armazenador {
         const valorComoString = JSON.stringify(valor);
         localStorage.setItem(chave, valorComoString);
     }
+    // o tipo T é um tipo genérico, o que significa que quando formos chamar este método nós vamos definir o tipo que queremos desse retorno. Esse T está aqui para representar algo genérico, não precisa ser necessariamente a letra T, mas o T é usado para fazer a associação com TIPO
     static obter(chave, reviver) {
         const valor = localStorage.getItem(chave);
         if (valor === null) {
--- projeto-bytebank/src/utils/Armazenador.ts
+++ projeto-bytebank/src/utils/Armazenador.ts
@@ -6,19 +6,20 @@ export class Armazenador {
     localStorage.setItem(chave, valorComoString);
   }
 
-  static obter(
+  // o tipo T é um tipo genérico, o que significa que quando formos chamar este método nós vamos definir o tipo que queremos desse retorno. Esse T está aqui para representar algo genérico, não precisa ser necessariamente a letra T, mas o T é usado para fazer a associação com TIPO
+  static obter<T>(
     chave: string,
     reviver?: (this: any, key: string, value: any) => any
-  ) {
+  ): T | null {
     const valor = localStorage.getItem(chave);
     if (valor === null) {
       return null;
     }
 
     if (reviver) {
-      return JSON.parse(valor, reviver);
+      return JSON.parse(valor, reviver) as T;
     }
 
-    return JSON.parse(valor);
+    return JSON.parse(valor) as T;
   }
 }
